c7cc276db700ca283eb459e7693d5ac0d50aa437
sttie <sttiemath@gmail.com>
2023-02-23 23:10:13 +0300

    more design

--- app/src/main/java/com/etu/bookcrossing/ui/auth/register/RegisterComposable.kt
+++ app/src/main/java/com/etu/bookcrossing/ui/auth/register/RegisterComposable.kt
@@ -1,70 +1,94 @@
 package com.etu.bookcrossing.ui.auth
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.etu.bookcrossing.R
 import com.etu.bookcrossing.ui.auth.register.REGISTRATION_SUCCEED_COMPOSABLE_ID
 
 const val REGISTER_COMPOSABLE_ID = "register_screen"
 
 @Composable
 fun RegisterComposable(navController: NavHostController) {
-    var name by rememberSaveable { mutableStateOf("") }
-    var surname by rememberSaveable { mutableStateOf("") }
-    var email by rememberSaveable { mutableStateOf("") }
-    var phone by rememberSaveable { mutableStateOf("") }
-    var password by rememberSaveable { mutableStateOf("") }
-    var password_confirm by rememberSaveable { mutableStateOf("") }
+    Column(modifier = Modifier.padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        var name = remember { mutableStateOf(TextFieldValue()) }
+        var surname = remember { mutableStateOf(TextFieldValue()) }
+        var email = remember { mutableStateOf(TextFieldValue()) }
+        var phone = remember { mutableStateOf(TextFieldValue()) }
+        var password = remember { mutableStateOf(TextFieldValue()) }
+        var password_confirm = remember { mutableStateOf(TextFieldValue()) }
 
-    Column {
+        Spacer(modifier = Modifier.height(20.dp))
         TextField(
-            value = name,
-            onValueChange = { name = it },
-            label = { Text("Name") }
-        )
-        TextField(
-            value = surname,
-            onValueChange = { surname = it },
-            label = { Text("Surname") }
-        )
-        TextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email") }
-        )
-        TextField(
-            value = phone,
-            onValueChange = { phone = it },
-            label = { Text("Phone") }
+            label = { Text(text = "Name") },
+            value = name.value,
+            onValueChange = { name.value = it }
         )
 
+        Spacer(modifier = Modifier.height(20.dp))
         TextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Enter password") },
-            visualTransformation = PasswordVisualTransformation(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
-        )
-        TextField(
-            value = password_confirm,
-            onValueChange = { password_confirm = it },
-            label = { Text("Enter password") },
-            visualTransformation = PasswordVisualTransformation(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+            label = { Text(text = "Surname") },
+            value = surname.value,
+            onValueChange = { surname.value = it }
         )
 
-        Button(onClick = { navController.navigate(REGISTRATION_SUCCEED_COMPOSABLE_ID) }) {
-            Text(text = "Sign up")
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Email") },
+            value = email.value,
+            onValueChange = { email.value = it }
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Phone") },
+            value = phone.value,
+            onValueChange = { phone.value = it }
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Password") },
+            value = password.value,
+            onValueChange = { password.value = it }
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Password confirm") },
+            value = password_confirm.value,
+            onValueChange = { password_confirm.value = it }
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+        Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+            Button(
+                onClick = { navController.navigate(REGISTRATION_SUCCEED_COMPOSABLE_ID) },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = colorResource(R.color.dark_green)
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text("Sign up")
+            }
         }
     }
 }
--- app/src/main/java/com/etu/bookcrossing/ui/auth/register/RegistrationSucceedComposable.kt
+++ app/src/main/java/com/etu/bookcrossing/ui/auth/register/RegistrationSucceedComposable.kt
@@ -1,20 +1,50 @@
 package com.etu.bookcrossing.ui.auth.register
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.etu.bookcrossing.R
 
 const val REGISTRATION_SUCCEED_COMPOSABLE_ID = "registration_succeed_screen"
 
 @Composable
 fun RegistrationSucceedComposable(navController: NavHostController) {
-    Column {
-        Text(text = "Here should be our logo")
-        Text(text = "Thank you for registration")
-        Button(onClick = { }) {
-            Text(text = "Start using")
+    Column(modifier = Modifier.padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "BookCrossing",
+            style = TextStyle(fontSize = 40.sp, fontFamily = FontFamily.Cursive)
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(text = "Thank you for registration!", fontSize = 14.sp, color = Color.Gray)
+
+        Spacer(modifier = Modifier.height(20.dp))
+        Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+            Button(
+                onClick = {  },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = colorResource(R.color.dark_green)
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text("Start using")
+            }
         }
     }
 }
