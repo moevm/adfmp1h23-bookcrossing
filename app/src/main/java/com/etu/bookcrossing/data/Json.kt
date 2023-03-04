7048ddac42314dbf28ab11a380a1b73ebde122ce
Daniil Kamakin <cursedowguy@gmail.com>
2023-03-04 21:42:33 +0300

    Implement first unit tests

--- app/src/main/java/com/etu/bookcrossing/data/Json.kt
+++ app/src/main/java/com/etu/bookcrossing/data/Json.kt
@@ -9,3 +9,11 @@ private val gson = Gson()
 fun <T> JsonReader.deserialize(type: TypeToken<T>): T {
     return gson.fromJson(this, type)
 }
+
+fun <T> T.serialize(): String {
+    return gson.toJson(this)
+}
+
+fun <T> T.jsonReader(): JsonReader {
+    return gson.newJsonReader(this.serialize().reader())
+}
