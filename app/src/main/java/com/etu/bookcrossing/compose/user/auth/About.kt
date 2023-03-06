8ead0da1d1153d187b1fe074392b52020dd50b26
Daniil Kamakin <cursedowguy@gmail.com>
2023-03-06 08:17:44 +0300

    small refactoring

--- app/src/main/java/com/etu/bookcrossing/compose/user/auth/About.kt
+++ app/src/main/java/com/etu/bookcrossing/compose/user/auth/About.kt
@@ -2,24 +2,14 @@ package com.etu.bookcrossing.compose.about
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.TextFieldValue
 import com.etu.bookcrossing.R
-import com.etu.bookcrossing.compose.common.BookCrossingTextField
-import com.etu.bookcrossing.compose.common.CursiveBigText
 
 @Composable
 fun About() {
