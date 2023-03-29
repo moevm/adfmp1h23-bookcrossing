3426242d95898af082ccbcb691a1384709f13777
sttie <sttiemath@gmail.com>
2023-03-30 01:45:06 +0300

    add BookPointTest

---
+++ app/src/androidTest/java/com/etu/bookcrossing/compose/map/BookPointTest.kt
@@ -0,0 +1,27 @@
+package com.etu.bookcrossing.compose.map
+
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.etu.bookcrossing.BaseTest
+import org.junit.Test
+
+class BookPointTest : BaseTest() {
+    var called = false
+
+    private fun onTakeBook(formattedTakenMessage: String, undoMessage: String,
+                   performed: () -> Unit, dismissed: () -> Unit) {
+        called = true
+        assert(formattedTakenMessage == "takenMessage!")
+        assert(undoMessage == "undoMessage!")
+    }
+
+    @Test
+    fun getAboutScreen() {
+        composeTestRule.setContent {
+            TakeBookItem("name!", "takenMessage!", "undoMessage!", ::onTakeBook)
+        }
+
+        composeTestRule.onNodeWithText("Take").performClick()
+        assert(called)
+    }
+}
