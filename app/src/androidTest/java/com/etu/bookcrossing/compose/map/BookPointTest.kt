a31e570591df28fdaea3ddbd186578116dd33f9b
sttie <sttiemath@gmail.com>
2023-03-30 01:50:53 +0300

    add another test to BookPointTest

--- app/src/androidTest/java/com/etu/bookcrossing/compose/map/BookPointTest.kt
+++ app/src/androidTest/java/com/etu/bookcrossing/compose/map/BookPointTest.kt
@@ -6,11 +6,12 @@ import com.etu.bookcrossing.BaseTest
 import org.junit.Test
 
 class BookPointTest : BaseTest() {
-    var called = false
+    var calledOnTakeBook = false
+    var calledAddNewBook = false
 
     private fun onTakeBook(formattedTakenMessage: String, undoMessage: String,
                    performed: () -> Unit, dismissed: () -> Unit) {
-        called = true
+        calledOnTakeBook = true
         assert(formattedTakenMessage == "takenMessage!")
         assert(undoMessage == "undoMessage!")
     }
@@ -22,6 +23,16 @@ class BookPointTest : BaseTest() {
         }
 
         composeTestRule.onNodeWithText("Take").performClick()
-        assert(called)
+        assert(calledOnTakeBook)
+    }
+
+    @Test
+    fun addNewBook() {
+        composeTestRule.setContent {
+            AddNewBook("name!") { calledAddNewBook = true }
+        }
+
+        composeTestRule.onNodeWithText("Add new book").performClick()
+        assert(calledAddNewBook)
     }
 }
