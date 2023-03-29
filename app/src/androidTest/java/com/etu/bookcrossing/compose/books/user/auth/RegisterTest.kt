136b7444e33cd1126e744ed72be95c4ca2820495
sttie <sttiemath@gmail.com>
2023-03-30 01:29:42 +0300

    add RegisterTest

--- app/src/androidTest/java/com/etu/bookcrossing/compose/books/user/auth/RegisterTest.kt
+++ app/src/androidTest/java/com/etu/bookcrossing/compose/books/user/auth/RegisterTest.kt
@@ -1,2 +1,23 @@
 package com.etu.bookcrossing.compose.books.user.auth
 
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.etu.bookcrossing.BaseTest
+import com.etu.bookcrossing.compose.user.auth.Register
+import org.junit.Test
+
+class RegisterTest : BaseTest() {
+    var calledOnRegister = false
+    var calledRegister = false
+    var calledAbout = false
+
+    @Test
+    fun getAboutScreen() {
+        composeTestRule.setContent {
+            Register(onRegister = { calledOnRegister = true })
+        }
+
+        composeTestRule.onNodeWithText("Sign up").performClick()
+        assert(calledOnRegister)
+    }
+}
